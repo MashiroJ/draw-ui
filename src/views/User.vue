4f54917d10b6e966290fe0efe4f7585193147a7a
Mashiro <1146446176@qq.com>
2024-09-20 20:11:05 +0800

    新增用户实现

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {getCurrentInstance, onMounted, reactive, ref} from 'vue';
-import {ElMessage,ElMessageBox} from 'element-plus'
+import {ElMessage, ElMessageBox} from 'element-plus'
 
 const {proxy} = getCurrentInstance();
 
@@ -60,9 +60,103 @@ const handleCurrentChange = (page) => {
   config.page = page
   getUserData()
 }
-const handleDelete = (val)=>{
-  ElMessageBox.confirm("是否删除该用户？").then(async ()=>{
-    await proxy.$api.deleteUser({id:val.id});
+
+//新增和编辑共用一个窗口，所以通过设置action区分
+const action = ref("add")
+//控制对话框是否显示
+const dialogVisible = ref(false)
+
+const formUser = reactive({
+  name: 'test',
+  sex: '1',
+
+})
+//表单校验规则
+const rules = reactive({
+  name: [{required: true, message: "姓名是必填项", trigger: "blur"}],
+  age: [
+    {required: true, message: "年龄是必填项", trigger: "blur"},
+    {type: "number", message: "年龄必须是数字"},
+  ],
+  sex: [{required: true, message: "性别是必选项", trigger: "change"}],
+  birth: [{required: true, message: "出生日期是必选项"}],
+  addr: [{required: true, message: '地址是必填项'}]
+})
+
+// 新增按钮点击事件
+const handleAdd = () => {
+  action.value = "add"
+  //打开对话窗
+  dialogVisible.value = true
+}
+
+//对话框右上角的关闭事件
+const handleClose = () => {
+  // 获取到表单dom，执行resetFields重置表单
+  proxy.$refs["userForm"].resetFields()
+  //关闭对话框
+  dialogVisible.value = false
+}
+
+//对话框右下角的取消事件
+const handleCancel = () => {
+  // 获取到表单dom，执行resetFields重置表单
+  proxy.$refs["userForm"].resetFields()
+  dialogVisible.value = false
+}
+
+const onSubmit = () => {
+  //执行userForm表单的validate进行规则校验，传入一个回调函数，回调函数会接受到一个是否校验通过的变量
+  proxy.$refs["userForm"].validate(async (valid) => {
+
+    //如果校验成功
+    if (valid) {
+      //res用于接收添加用户或者编辑用户接口的返回值
+      let res = null
+      //如果当前的操作是新增，则调用新增接口
+      if (action.value == "add") {
+        res = await proxy.$api.addUser(formUser);
+      } else if (action.value == "edit") {
+        //在之前的onSubmit方法中增加的代码
+        //如果是编辑
+        res = await proxy.$api.editUser(formUser)
+      }
+      //如果接口调用成功
+      if (res) {
+        //关闭对话框，重置表单，重新请求用户数据
+        dialogVisible.value = false;
+        proxy.$refs["userForm"].resetFields()
+        getUserData()
+      }
+      //如果校验失败
+    } else {
+      ElMessage({
+        showClose: true,
+        message: "请输入正确的内容",
+        type: "error",
+      })
+    }
+
+  })
+}
+
+//格式化日期，格式化为：1997-01-02这种
+const timeFormat = (time) => {
+  var time = new Date(time);
+  var year = time.getFullYear();
+  var month = time.getMonth() + 1;
+  var date = time.getDate();
+
+  function add(m) {
+    return m < 10 ? "0" + m : m;
+  }
+
+  return year + "-" + add(month) + "-" + add(date);
+}
+
+const handleDelete = (val) => {
+  ElMessageBox.confirm("是否删除该用户？").then(async () => {
+    await proxy.$api.deleteUser({id: val.id});
     ElMessage({
       showClose: true,
       message: '删除成功',
@@ -80,7 +174,7 @@ onMounted(() => {
 
 <template>
   <div class="user-header">
-    <el-button type="primary">新增</el-button>
+    <el-button type="primary" @click="handleAdd">新增</el-button>
     <el-form :inline="true" :model="fromInline" class="user-header-form">
       <el-form-item>
         <el-input placeholder="请输入用户名"
@@ -119,27 +213,90 @@ onMounted(() => {
                    @size-change="handleSizeChange"
                    @current-change="handleCurrentChange"></el-pagination>
   </div>
+  <el-dialog
+      v-model="dialogVisible"
+      :title="action == 'add' ? '新增用户' : '编辑用户'"
+      width="35%"
+      :before-close="handleClose"
+  >
+    <!--需要注意的是设置了:inline="true"，
+ 会对el-select的样式造成影响，我们通过给他设置一个class=select-clearn
+ 在css进行处理-->
+    <el-form :inline="true" :model="formUser" :rules="rules" ref="userForm">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="formUser.name" placeholder="请输入姓名"/>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="年龄" prop="age">
+            <el-input v-model.number="formUser.age" placeholder="请输入年龄"/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item class="select-clearn" label="性别" prop="sex">
+            <el-select v-model="formUser.sex" placeholder="请选择">
+              <el-option label="男" value="1"/>
+              <el-option label="女" value="0"/>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="出生日期" prop="birth">
+            <el-date-picker
+                v-model="formUser.birth"
+                type="date"
+                placeholder="请输入"
+                style="width: 100%"
+                value-format="yyyy-MM-dd"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-form-item
+            label="地址"
+            prop="addr"
+        >
+          <el-input v-model="formUser.addr" placeholder="请输入地址"/>
+        </el-form-item>
+      </el-row>
+      <el-row style="justify-content: flex-end">
+        <el-form-item>
+          <el-button type="primary" @click="handleCancel">取消</el-button>
+          <el-button type="primary" @click="onSubmit">确定</el-button>
+        </el-form-item>
+      </el-row>
+    </el-form>
+  </el-dialog>
 </template>
 
 <style scoped lang="less">
-.user-header{
-  display:flex;
-  justify-content:space-between;
+.user-header {
+  display: flex;
+  justify-content: space-between;
 }
-.table{
-  position:relative;
-  height:520px;
-  .pager{
-    position:absolute;
-    right:10px;
-    bottom:30px;
+
+.table {
+  position: relative;
+  height: 520px;
+
+  .pager {
+    position: absolute;
+    right: 10px;
+    bottom: 30px;
   }
-  .el-table{
-    width:100%;
-    height:500px;
+
+  .el-table {
+    width: 100%;
+    height: 500px;
   }
 }
-.select-clearn{
-  display:flex;
+
+.select-clearn {
+  display: flex;
 }
 </style>
